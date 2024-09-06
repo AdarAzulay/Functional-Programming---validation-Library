7f8fc2be37ee5e05fb136a6fac5570e8dffea2e4
Adar <adar9800@gmail.com>
2024-09-06 13:06:30 +0300

    first comment in the branch

--- src/main/scala/il/ac/hit/validation/User.scala
+++ src/main/scala/il/ac/hit/validation/User.scala
@@ -2,4 +2,5 @@ package il.ac.hit.validation
 
 class User(val username: String, val email: String, val password: String, val age: Int) {
   // Primary constructor initializes all the properties
+  //another comment here !
 }
